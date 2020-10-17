821a9e8d2b50af80a5e6aada110b7e2891322068
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2020-10-17 02:24:10 +0100

    Parse various parts of request appropiately

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const http = require('http');
+const { StringDecoder } = require('string_decoder');
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
+  const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/$/g, '');
+
+  const method = req.method;
+  const requestQuery = parsedUrl.searchParams;
+
+  const headers  = req.headers;
+
+  const decoder = new StringDecoder('utf8');
+  let buffer = '';
+
+  req.on('data', (chunk) => {
+    buffer += decoder.write(chunk);
+  });
+
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    res.end('Hello World\n');
+
+    console.log('Request recieved with this payload: \n', buffer);
+  })
 });
 
 server.listen(PORT, () => {
